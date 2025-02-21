bb3b36c68c19693c0203f68c88e835808916a99c
PritRaj1 <prithvi.raj1@outlook.com>
2025-02-20 22:03:27 -0800

    darcy

--- plotting/results/plot_darcy.py
+++ plotting/results/plot_darcy.py
@@ -2,10 +2,20 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.rcParams.update({
+    'text.usetex': True, 
+    'font.family': 'serif', 
+    'font.serif': ['Compute Modern'], 
+    'axes.unicode_minus': False, 
+    'text.latex.preamble': r'\usepackage{amsmath} \usepackage{amsfonts} \usepackage{amssymb} \usepackage{bm} \newcommand{\probP}{\text{I\kern-0.15em P}}'  
+})
+
 # File paths to HDF5 files
+prior = 'lognormal'
+
 file_paths = [
-    'logs/gaussian_RBF/MNIST_1/generated_images.h5',
-    'logs/gaussian_FFT/DARCY_FLOW_1/generated_images.h5'
+    f'logs/{prior}_RBF/DARCY_FLOW_1/generated_images.h5',
+    f'logs/{prior}_FFT/DARCY_FLOW_1/generated_images.h5'
 ]
 
 images = []
@@ -16,6 +26,8 @@ for file_path in file_paths:
 grid_size = (10, 10)  
 fig, axes = plt.subplots(grid_size[0], grid_size[1] * 2, figsize=(16, 8))
 
+titles = ['RBF', 'FFT']
+
 for dataset_idx, image_set in enumerate(images):
     for i in range(grid_size[0] * grid_size[1]):
         row, col = divmod(i, grid_size[1])
@@ -24,12 +36,15 @@ for dataset_idx, image_set in enumerate(images):
         
         img = np.transpose(image_set[i, :, :, :], (1, 2, 0))
         
-        ax.imshow(img, cmap='gray')
+        ax.imshow(img)
         ax.axis('off')  
+    
+    # Set title for each grid
+    axes[0, dataset_idx * grid_size[1] + 4].set_title(titles[dataset_idx], fontsize=12, pad=20)
 
 plt.subplots_adjust(wspace=0, hspace=0)
-# plt.show()
-plt.savefig('figures/results/nist.png')
+plt.show()
+# plt.savefig(f'figures/results/farcy_{prior}.png')
 
 # Does not make sense to use FID/KID for MNIST/FMNIST
 
