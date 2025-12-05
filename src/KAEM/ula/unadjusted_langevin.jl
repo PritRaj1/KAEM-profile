aa81b2b25a30ee9451af228afdaf84761c572c03
PritRaj1 <prithvi.raj1@outlook.com>
2025-12-05 03:49:13 -0800

    explicit separate

--- src/KAEM/ula/unadjusted_langevin.jl
+++ src/KAEM/ula/unadjusted_langevin.jl
@@ -164,7 +164,7 @@ function (sampler::ULA_sampler)(
 
             z_init, st_ebm = begin
                 if model.prior.bool_config.mixture_model
-                    sample_mixture(
+                    z, st = sample_mixture(
                         model_copy.prior,
                         ps.ebm,
                         st_kan.ebm,
@@ -174,7 +174,7 @@ function (sampler::ULA_sampler)(
                         ula_init = true
                     )
                 else
-                    sample_univariate(
+                    z, st = sample_univariate(
                         model_copy.prior,
                         ps.ebm,
                         st_kan.ebm,
