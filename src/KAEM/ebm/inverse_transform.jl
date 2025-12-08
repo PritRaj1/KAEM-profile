aaa1f5983bc0f92002105267a5fecd107d266150
PritRaj1 <prithvi.raj1@outlook.com>
2025-12-07 19:53:11 -0800

    simplified

--- src/KAEM/ebm/inverse_transform.jl
+++ src/KAEM/ebm/inverse_transform.jl
@@ -14,12 +14,7 @@ function interpolate_kernel(cdf, grid, rand_vals, G; mix_bool = false)
     grid = cat(grid, view(grid, :, :, G:G); dims = 3) # Repeat end, so G + 1 indexes final
 
     # First index, i, such that cdf[i] >= rand_vals
-    rv = (
-        mix_bool ?
-            rand_vals :
-            PermutedDimsArray(view(rand_vals, :, :, :, :), (1, 2, 4, 3))
-    )
-    indices = sum(1 .+ (cdf .< rv); dims = 3)
+    indices = sum(1 .+ (cdf .< rand_vals); dims = 3)
     first_bool = indices .== 1 |> Lux.f32
     mask2 = indices .== grid_idxs |> Lux.f32
     mask1 = mask2 .- 1.0f0
@@ -39,15 +34,9 @@ function interpolate_kernel(cdf, grid, rand_vals, G; mix_bool = false)
     c2 = dropdims(sum(mask2 .* cdf; dims = 3); dims = 3)
     rv = mix_bool ? dropdims(rand_vals; dims = 3) : rand_vals
 
-    z_length = z2 - z1
     cdf_length = c2 - c1
-    logical_or = (
-        (z_length .== 0) .+
-            (cdf_length .== 0) .> 0
-    )
-
     return ifelse.(
-        logical_or,
+        cdf_length .== 0,
         z1,
         z1 .+ (z2 .- z1) .* ((rv .- c1) ./ cdf_length)
     )
@@ -72,7 +61,7 @@ function sample_univariate(
     z = interpolate_kernel(
         cdf,
         PermutedDimsArray(view(grid, :, :, :), (3, 1, 2)),
-        rand_vals,
+        PermutedDimsArray(view(rand_vals, :, :, :, :), (1, 2, 4, 3)),
         ebm.N_quad,
     )
     return z, st_lyrnorm_new
