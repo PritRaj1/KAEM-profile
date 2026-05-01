3defd69593113c0a819eb01ca90fa1515e3fe0b0
PritRaj1 <prithvi.raj1@outlook.com>
2026-05-01 16:56:03 -0700

    reactant its rewrite

--- src/KAEM/ebm/inverse_transform.jl
+++ src/KAEM/ebm/inverse_transform.jl
@@ -2,69 +2,52 @@ module InverseTransformSampling
 
 export UnivITSSampler, MixITSSampler
 
-using LinearAlgebra, ComponentArrays, Lux
+using LinearAlgebra, ComponentArrays, Lux, Reactant
 
 using ..Utils
 
 include("mixture_selection.jl")
 using .MixtureChoice: choose_component
 
-_init_rv(::MixtureMode, rand_vals) = rand_vals
-_init_rv(::UnivariateMode, rand_vals) =
-    PermutedDimsArray(view(rand_vals, :, :, :, :), (1, 2, 4, 3))
+function dotprod_attn(Q, K, z, scale, min_z, max_z, q_size, s_size)
+    z = reshape(z, q_size, 1, s_size) .* ((max_z - min_z) + min_z)
+    return dropdims(sum((Q .* z) .* (K .* z); dims = 3); dims = 3) ./ scale
+end
 
-_final_rv(::MixtureMode, rand_vals) = dropdims(rand_vals; dims = 3)
-_final_rv(::UnivariateMode, rand_vals) = rand_vals
-
-function interpolate_kernel(
-        cdf, grid, rand_vals, G;
-        mode::AbstractSamplingMode = UnivariateMode(),
-    )
-    grid_idxs = reshape(1:(G + 1), 1, 1, G + 1, 1)
-    grid = cat(grid, view(grid, :, :, G:G); dims = 3) # Repeat end, so G + 1 indexes final
-
-    # First index, i, such that cdf[i] >= rand_vals
-    rv = _init_rv(mode, rand_vals)
-    indices = sum(cdf .< rv; dims = 3) .+ 1
-    first_bool = indices .== 1 |> Lux.f32
-    mask2 = indices .== grid_idxs |> Lux.f32
-    mask1 = (indices .- 1) .== grid_idxs |> Lux.f32
-
-    z1 = dropdims(
-        (first_bool .* grid[:, :, 1]) .+
-            (1.0f0 .- first_bool) .*
-            sum(mask1 .* grid; dims = 3); dims = 3
-    )
-    z2 = dropdims(sum(mask2 .* grid; dims = 3); dims = 3)
-
-    c1 = dropdims(
-        (first_bool .* 0.0f0) .+
-            (1.0f0 .- first_bool) .*
-            sum(mask1 .* cdf; dims = 3); dims = 3
-    )
-    c2 = dropdims(sum(mask2 .* cdf; dims = 3); dims = 3)
-    rv = _final_rv(mode, rand_vals)
-
-    cdf_length = c2 - c1
+# Linear interp z(rv) on bin (z1,c1)->(z2,c2)
+function _lerp(z1, z2, c1, c2, rv)
+    len = c2 .- c1
+    safe = ifelse.(len .== 0, 1.0f0, len)
     return ifelse.(
-        cdf_length .== 0,
+        len .== 0,
         z1,
-        z1 .+ (z2 .- z1) .* ((rv .- c1) ./ cdf_length)
+        z1 .+ (z2 .- z1) .* ((rv .- c1) ./ safe),
     )
 end
 
-function dotprod_attn(
-        Q,
-        K,
-        z,
-        scale,
-        min_z,
-        max_z,
-        q_size,
-        s_size
-    )
-    z = reshape(z, q_size, 1, s_size) .* ((max_z - min_z) + min_z)
-    return dropdims(sum((Q .* z) .* (K .* z); dims = 3); dims = 3) ./ scale
+function _its_step(cdf, grid, rv, Q, M, B, mixture::Bool)
+    jh = Reactant.Ops.findfirst(cdf .>= rv; dimension = 3)
+    jl = max.(jh .- Int64(1), Int64(1))
+
+    Mtot = Q * M * B
+    qcol = reshape(Reactant.Ops.iota(Int64, [Q, M, B]; iota_dimension = 1), Mtot, 1)
+    mcol = reshape(Reactant.Ops.iota(Int64, [Q, M, B]; iota_dimension = 2), Mtot, 1)
+    bcol = reshape(Reactant.Ops.iota(Int64, [Q, M, B]; iota_dimension = 3), Mtot, 1)
+    one = Reactant.Ops.fill(Int64(1), [Mtot, 1])
+    jhc, jlc = reshape(jh, Mtot, 1), reshape(jl, Mtot, 1)
+
+    cdf_b = mixture ? bcol : one
+    grid_q = mixture ? qcol : one
+
+    out = (Q, M, B)
+    gather(src, idx) = reshape(Reactant.Ops.gather_getindex(src, idx), out)
+
+    z2 = gather(grid, hcat(grid_q, mcol, jhc, one))
+    z1 = gather(grid, hcat(grid_q, mcol, jlc, one))
+    c2 = gather(cdf, hcat(qcol, mcol, jhc, cdf_b))
+    c1 = gather(cdf, hcat(qcol, mcol, jlc, cdf_b))
+
+    return _lerp(z1, z2, c1, c2, dropdims(rv; dims = 3))
 end
 
 struct UnivITSSampler{E}
@@ -73,21 +56,26 @@ end
 
 function (s::UnivITSSampler)(ps, st_kan, st_lux, st_rng; ula_init = false)
     ebm = s.ebm
+    Q, P, B, G = ebm.q_size, ebm.p_size, ebm.s_size, ebm.N_quad
+
     cdf, grid, st_lyrnorm_new = ebm.quad(
         ebm, ps.ebm, st_kan.ebm, Lux.testmode(st_lux.ebm), st_kan.quad,
     )
     cdf = cumsum(cdf; dims = 3)
     cdf = cat(view(zero(cdf), :, :, 1:1), cdf; dims = 3)
 
+    zmin = ebm.bool_config.no_grid ? st_kan.ebm[:a].min : view(st_kan.ebm[:a].grid, :, 1)
+    grid = PermutedDimsArray(view(grid, :, :, :), (3, 1, 2))
+    grid = cat(reshape(zmin, 1, P, 1), grid; dims = 3)
+
     rv = ula_init ? st_rng.posterior_its : st_rng.prior_its
     rand_vals = rv .* cdf[:, :, end]
-    z = interpolate_kernel(
-        cdf,
-        PermutedDimsArray(view(grid, :, :, :), (3, 1, 2)),
-        rand_vals,
-        ebm.N_quad,
-    )
-    return z, st_lyrnorm_new
+
+    cdf_4d = reshape(cdf, Q, P, G + 1, 1)
+    grid_4d = reshape(grid, 1, P, G + 1, 1)
+    rv_4d = reshape(rand_vals, Q, P, 1, B)
+
+    return _its_step(cdf_4d, grid_4d, rv_4d, Q, P, B, false), st_lyrnorm_new
 end
 
 struct MixITSSampler{E}
@@ -96,48 +84,33 @@ end
 
 function (s::MixITSSampler)(ps, st_kan, st_lux, st_rng; ula_init = false)
     ebm = s.ebm
-    alpha = ps.ebm.dist.α .* 1.0f0
-    if ebm.bool_config.use_attention_kernel
-        scale = sqrt(Float32(ebm.s_size))
-        alpha = dotprod_attn(
-            ps.ebm.attention.Q,
-            ps.ebm.attention.K,
-            st_rng.attn_rand,
-            scale,
-            st_kan.ebm[:a].min,
-            st_kan.ebm[:a].max,
-            ebm.q_size,
-            ebm.s_size,
-        )
-    end
-    mask = choose_component(
-        alpha,
-        ebm.s_size,
-        ebm.q_size,
-        ebm.p_size,
-        st_rng;
-        ula_init = ula_init,
-    )
+    Q, P, B, G = ebm.q_size, ebm.p_size, ebm.s_size, ebm.N_quad
+
+    alpha = ebm.bool_config.use_attention_kernel ?
+        dotprod_attn(
+            ps.ebm.attention.Q, ps.ebm.attention.K, st_rng.attn_rand,
+            sqrt(Float32(B)), st_kan.ebm[:a].min, st_kan.ebm[:a].max, Q, B,
+        ) : ps.ebm.dist.α .* 1.0f0
+    mask = choose_component(alpha, B, Q, P, st_rng; ula_init = ula_init)
+
     cdf, grid, st_lyrnorm_new = ebm.quad(
         ebm, ps.ebm, st_kan.ebm, Lux.testmode(st_lux.ebm), st_kan.quad;
-        mode = MixtureMode(),
-        component_mask = mask,
+        mode = MixtureMode(), component_mask = mask,
     )
 
     cdf = cumsum(cdf; dims = 3)
     cdf = PermutedDimsArray(view(cdf, :, :, :, :), (1, 4, 3, 2))
     cdf = cat(view(zero(cdf), :, :, 1:1, :), cdf; dims = 3)
 
+    zmin = ebm.bool_config.no_grid ? st_kan.ebm[:a].min : view(st_kan.ebm[:a].grid, :, 1)
+    grid = PermutedDimsArray(view(grid, :, :, :), (1, 3, 2))
+    grid = cat(reshape(zmin, Q, 1, 1), grid; dims = 3)
+
     rv = ula_init ? st_rng.posterior_its : st_rng.prior_its
     rand_vals = rv .* cdf[:, :, end:end, :]
-    z = interpolate_kernel(
-        cdf,
-        PermutedDimsArray(view(grid, :, :, :), (1, 3, 2)),
-        rand_vals,
-        ebm.N_quad;
-        mode = MixtureMode(),
-    )
-    return z, st_lyrnorm_new
+    grid_4d = reshape(grid, Q, 1, G + 1, 1)
+
+    return _its_step(cdf, grid_4d, rand_vals, Q, 1, B, true), st_lyrnorm_new
 end
 
 end
--- src/KAEM/ebm/mixture_selection.jl
+++ src/KAEM/ebm/mixture_selection.jl
@@ -3,46 +3,21 @@ module MixtureChoice
 export choose_component
 
 using NNlib: softmax
-using LinearAlgebra, Lux
+using LinearAlgebra, Lux, Reactant
 
 using ..Utils
 
-function mask_kernel(
-        α,
-        rand_vals,
-        Q,
-        P
-    )
-    """One-hot mask for chosen index"""
-    indices = sum(α .< rand_vals; dims = 2) .+ 1
-    p_range = reshape(1:P, 1, P, 1)
-    mask = indices .== p_range |> Lux.f32
-    return mask
+function mask_kernel(α, rand_vals, Q, P, S)
+    """One-hot mask at j = first p s.t. α[q,p] ≥ rv."""
+    j = Reactant.Ops.findfirst(α .>= rand_vals; dimension = 2)
+    return reshape(j, Q, 1, S) .== reshape(1:P, 1, P, 1) |> Lux.f32
 end
 
-function choose_component(
-        α,
-        num_samples,
-        q_size,
-        p_size,
-        st_rng;
-        ula_init = false
-    )
-    """
-    Creates a one-hot mask for mixture model, q, to select one component, p.
-
-    Args:
-        alpha: The mixture proportions, (q, p).
-        num_samples: The number of samples to generate.
-        q_size: The number of mixture models.
-        rng: The random number generator.
-
-    Returns:
-        chosen_components: The one-hot mask for each mixture model, (num_samples, q, p).    
-    """
+function choose_component(α, num_samples, q_size, p_size, st_rng; ula_init = false)
+    """One-hot (Q, P, num_samples) mask selecting one mixture component per (q, sample)."""
     rand_vals = ula_init ? st_rng.mix_rv_mcmc : st_rng.mix_rv
     α = cumsum(softmax(α; dims = 2); dims = 2)
-    return mask_kernel(α, rand_vals, q_size, p_size)
+    return mask_kernel(α, rand_vals, q_size, p_size, num_samples)
 end
 
 end
